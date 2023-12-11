3115706918d80e716eea1f110d12e3a395ff63f9
Air-KS <kevinrogeret@gmail.com>
2023-12-11 17:31:34 +0000

    Add def markdown_to_html

--- markdown2html.py
+++ markdown2html.py
@@ -11,7 +11,7 @@ import os
 
 def file_exist(argv):
     """Verify if the necessary files exist and if the correct number of arguments are provided.
-    
+
     Args:
         argv (list of str): Command line arguments where argv[1] is the source Markdown file
                             and argv[2] is the destination HTML file.
@@ -28,5 +28,35 @@ def file_exist(argv):
     sys.exit(0)
 
 
+def markdown_to_html(md_file, html_file):
+    """Defines a function to convert a Markdown file to HTML.
+
+    md_file: path to the Markdown file to convert.
+    html_file: path to the output HTML file.
+    """
+    with open(md_file, 'r') as f:
+        lines = f.readlines(0)
+
+    with open(html_file, 'w') as f:
+        """
+        Opens the HTML file for writing.
+        Any existing content will be replaced.
+        """
+
+        for index in lines:
+            index = index.strip()
+
+            if index.startswith('#'):
+                """
+                If the line is a Markdown title
+                (starts with '#'), convert.
+                """
+                level = index.count('#')
+                heading = index.strip('#').strip()
+                html_heading = f'<h{level}>{heading}/h{level}>'
+                f.write(html_heading + '\n')
+
+
 if __name__ == "__main__":
     file_exist(sys.argv)
+    markdown_to_html(sys.argv[1], sys.argv[2])
