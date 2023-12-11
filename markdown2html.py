db212c8cee6d2de4c0fc9abf8cc1292c377e5c71
Air-KS <kevinrogeret@gmail.com>
2023-12-11 20:11:46 +0000

    Add <p> and <br/>

--- markdown2html.py
+++ markdown2html.py
@@ -24,58 +24,65 @@ def markdown_to_html(md_file, html_file):
             lines = f.readlines()
 
         with open(html_file, 'w') as f:
-            in_ul = False # For unordered lists
-            in_ol = False # For ordered lists
-            in_para = False # for paragraph
+            in_ul = False
+            in_ol = False
+            in_para = False
             
             for index in range(len(lines)):
                 line = lines[index].rstrip("\n")
                 next_line = lines[index + 1].rstrip("\n") if index + 1 < len(lines) else ''
                 
-                if line == '' and not in_para:
-                    continue
-
+                # Closing tags if needed before starting new ones
+                if line.startswith(('#', '-', '*')) and in_para:
+                    f.write('</p>\n')
+                    in_para = False
+                if line.startswith(('#', '')) and in_ul:
+                    f.write('</ul>\n')
+                    in_ul = False
+                if line.startswith(('#', '')) and in_ol:
+                    f.write('</ol>\n')
+                    in_ol = False
+                
+                # Processing lines
                 if line.startswith('#'):
-                    if in_para:
-                        f.write('</p>\n')
-                        in_para = False
-                    if in_ul:
-                        f.write('</ul>\n')
-                        in_ul = False
-                    if in_ol:
-                        f.write('</ol>\n')
-                        in_ol = False
-                        
                     level = line.count('#')
                     heading = line.strip('#').strip()
                     html_heading = f'<h{level}>{heading}</h{level}>'
                     f.write(html_heading + '\n')
-
                 elif line.startswith('-'):
                     if not in_ul:
                         f.write('<ul>\n')
                         in_ul = True
                     list_item = line.strip('-').strip()
                     f.write(f'<li>{list_item}</li>\n')
-
                 elif line.startswith('*'):
                     if not in_ol:
                         f.write('<ol>\n')
                         in_ol = True
                     list_item = line.strip('*').strip()
                     f.write(f'<li>{list_item}</li>\n')
-
-                else:
+                elif line:
                     if not in_para:
                         f.write('<p>\n')
                         in_para = True
-                    if next_line == '' or next_line.startswith('#') or next_line.startswith('-') or next_line.startswith('*'):
+                    if next_line == '' or next_line.startswith(('#', '-', '*')):
                         f.write(f'{line}\n')
-                        if in_para:
-                            f.write('</p>\n')
-                            in_para = False
+                        f.write('</p>\n')
+                        in_para = False
                     else:
                         f.write(f'{line}<br/>\n')
+                else:
+                    if in_para:
+                        f.write('</p>\n')
+                        in_para = False
+
+            # Close any open tags at the end of file
+            if in_ul:
+                f.write('</ul>\n')
+            if in_ol:
+                f.write('</ol>\n')
+            if in_para:
+                f.write('</p>\n')
 
     except IOError as e:
         print(f"Error: {e}", file=sys.stderr)
