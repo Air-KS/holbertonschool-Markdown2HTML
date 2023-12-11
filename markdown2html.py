90ddeeae3a76c432f4c283113981f153a1de47f4
Air-KS <kevinrogeret@gmail.com>
2023-12-11 17:00:09 +0000

    Markdown to HTML Converter

--- markdown2html.py
+++ markdown2html.py
@@ -1,26 +1,31 @@
 #!/usr/bin/python3
 """
 Markdown to HTML Converter
-Task 1: convert heading
+Task 0: convert heading
 """
 
+from os.path import isfile
 import sys
 import os
 
 
-def file_exist():
-    """convert file md to html task 0"""
-    if len(sys.argv) < 3:
+def file_exist(argv):
+    """Verify if the necessary files exist and if the correct number of arguments are provided.
+    
+    Args:
+        argv (list of str): Command line arguments where argv[1] is the source Markdown file
+                            and argv[2] is the destination HTML file.
+    """
+    if len(argv) < 3:
         print("Usage: ./markdown2html.py README.md README.html",
               file=sys.stderr)
         sys.exit(1)
 
-    markdown_file = sys.argv[1]
-    output_file = sys.argv[2]
-
-    if not os.path.exists(markdown_file):
-        print(f"Missing {markdown_file}", file=sys.stderr)
+    if isfile(argv[1]) is False:
+        print("Missing " + argv[1], file=sys.stderr)
         sys.exit(1)
+
+    sys.exit(0)
 
 
 if __name__ == "__main__":
